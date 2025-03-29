788244e45279605540609a7644d728db6d85117b
Mylena Andrade <mylenaandrade200@gmail.com>
2025-03-29 15:53:10 -0300

    feat: adicionar movimentação ao personagem

---
+++ Assets/Scripts/Personagem/ControladorPersonagem.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPersonagem : MonoBehaviour
+{
+    private Rigidbody2D personagemRigidbody2D;
+    //private Animator    personagemAnimator;
+    public float        personagemSpeed;
+    private Vector2     personagemDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        personagemRigidbody2D = GetComponent<Rigidbody2D>();
+       //personagemAnimator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        personagemDirection = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        if(personagemDirection.sqrMagnitude > 0.1)
+        {
+            //personagemAnimator.SetFloat("AxisX", personagemDirection.x);
+            
+            //personagemAnimator.SetInteger("Movement", 1);
+        }
+        else
+        {
+            //personagemAnimator.SetInteger("Movement", 0);
+        }
+        
+        personagemRigidbody2D.MovePosition(personagemRigidbody2D.position + personagemDirection * personagemSpeed * Time.fixedDeltaTime);
+
+    }
+
+    void Flip()
+    {
+        if(personagemDirection.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 0f);
+        }
+        else if(personagemDirection.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }  
+    }
+}
