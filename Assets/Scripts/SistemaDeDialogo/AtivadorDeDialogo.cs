8a239a91997b1c080a2ed8a803f89666b9b14050
Christian-Rui <chris2w0social@gmail.com>
2025-03-30 15:39:35 -0300

    feat: adicionar icones e dialogos ambientalistas

--- Assets/Scripts/SistemaDeDialogo/AtivadorDeDialogo.cs
+++ Assets/Scripts/SistemaDeDialogo/AtivadorDeDialogo.cs
@@ -1,8 +1,14 @@
+using System.Diagnostics;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
 
 public class DialogueActivator : MonoBehaviour, IInteracaoDialogo
 {
     [SerializeField] private ObjetoDialogo objetoDialogo;
+    [SerializeField] private GameObject aperteE;
+    private GameObject objetoArmazenado;
 
     public void AtualizarObjetoDialogo(ObjetoDialogo objetoDialogo)
     {
@@ -13,6 +19,10 @@ public class DialogueActivator : MonoBehaviour, IInteracaoDialogo
     {
         if (other.CompareTag("Player") && other.TryGetComponent(out ControladorPersonagem player))
         {
+            if(aperteE != null){
+                objetoArmazenado = Instantiate(aperteE, new Vector2(transform.position.x, transform.position.y + 5), Quaternion.identity);
+            }
+            
             player.InteracaoD = this;
         }
     }
@@ -21,6 +31,10 @@ public class DialogueActivator : MonoBehaviour, IInteracaoDialogo
     {
         if (other.CompareTag("Player") && other.TryGetComponent(out ControladorPersonagem player))
         {
+            if(objetoArmazenado != null){
+                Destroy(objetoArmazenado);
+            }
+            
             if (player.InteracaoD is DialogueActivator dialogueActivator && dialogueActivator == this)
             {
                 player.InteracaoD = null;
